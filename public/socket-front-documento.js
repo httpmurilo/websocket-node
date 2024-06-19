3b01e73496fef11d4a8a6c15420ecac2004bbce9
httpmurilo <muriloeduardo1997@outlook.com>
2024-06-18 21:58:34 -0300

    retornando o texto_editor

--- public/socket-front-documento.js
+++ public/socket-front-documento.js
@@ -5,13 +5,17 @@ const socket = io();
 
 
 function selecionarDocumento(nome) {
-    socket.emit("selecionar_documento", nome);
+    socket.emit("selecionar_documento", nome, (texto) => {
+        atualizaTextoEditor(texto);
+
+    });
 }
 
 
 function emitirTextoEditor(dados) {
     socket.emit("texto_editor", dados);
 }
+
 
 socket.on("texto_editor_clientes", (texto) => {
     atualizaTextoEditor(texto);
--- src/socket-back.js
+++ src/socket-back.js
@@ -1,15 +1,40 @@
 import io from "./servidor.js";
 
+const documentos = [
+    {
+        nome: "JavaScript",
+        texto: "texto de javascript"
+    },
+    {
+        nome: "Node",
+        texto: "texto de Node"
+    },
+    {
+        nome: "Socket.io",
+        texto: "texto de SocketIO"
+    }
+
+]
+
 
 io.on("connection", (socket) => {
     console.log("Um cliente se conectou :ID :", socket.id);
 
     
 
-        socket.on("selecionar_documento", (nomeDocumento) =>{
+        socket.on("selecionar_documento", (nomeDocumento, devolverTexto) =>{
             //cliente que ta conectado agora, coloca em uma sala, conceito do socket.io que vai agrupar clients
 
+
             socket.join(nomeDocumento);
+
+            const documento = encontrarDocumento(nomeDocumento);
+
+            if(documento) {
+                devolverTexto(documento.texto);
+            }
+            
+           
 
         } );
         
@@ -23,3 +48,10 @@ io.on("connection", (socket) => {
 
         });
 });
+
+function encontrarDocumento(nome) {
+    const documento  = documentos.find(() => {
+        return documento.nome === nome;
+    });
+    return documento;
+}
